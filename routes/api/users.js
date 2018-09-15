bcacf75e3f6268fd596be64aa34b8c36610916eb
Helfi Pangestu <hellfipangestu@gmail.com>
2018-09-15 12:26:42 +0700

    Add validation login and register

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load Input Validation
 const validateRegisterInput = require("../../validations/register");
+const validateLoginInput = require("../../validations/login");
 
 // Load User model
 const User = require("../../models/User");
@@ -64,6 +65,13 @@ router.post("/register", (req, res) => {
 // @desc    Login user / Returning JWT Token
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -71,7 +79,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found." });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check Password
@@ -92,7 +101,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password Incorect" });
+        errors.password = "Password incorect";
+        return res.status(400).json(errors);
       }
     });
   });
