67bf4b23afe61d8612297c8b9fe942221d1d50b1
Helfi Pangestu <hellfipangestu@gmail.com>
2018-09-23 16:54:35 +0700

    Add Login and jwt-auth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -10,6 +13,16 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
 import "./App.css";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user in isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
